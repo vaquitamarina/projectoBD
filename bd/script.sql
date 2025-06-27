34db7a4dd399a1b6b46c6ea3b4e3466ba344f2af
wiLLotheRock <wrcopajah@unjbg.edu.pe>
2025-06-27 12:05:56 -0500

    puse comas

--- bd/script.sql
+++ bd/script.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS usuario (
     idUsuario INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(10) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS pasajero (
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS pasajero (
     email VARCHAR(100) NOT NULL,
     fechaDeNacimiento DATE,
     sexo CHAR(1) NOT NULL,
-    dni CHAR(8) UNIQUE,   
+    dni CHAR(8) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS trabajador (
@@ -23,21 +23,21 @@ CREATE TABLE IF NOT EXISTS trabajador (
     nombres VARCHAR(50) NOT NULL,
     apellidos VARCHAR(50) NOT NULL,
     sexo CHAR(1) NOT NULL,
-    dni CHAR(8) UNIQUE,
+    dni CHAR(8) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS bus (
     placa VARCHAR(10) AUTO_INCREMENT PRIMARY KEY,
     clase VARCHAR(20) NOT NULL,
     estado VARCHAR(20) NOT NULL,
-    nAsientos INT NOT NULL, 
+    nAsientos INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS viaje (
     idViaje INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE NOT NULL,
     hInicio TIME NOT NULL,
-    hFinal TIME NOT NULL,
+    hFinal TIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS asiento (
@@ -45,13 +45,13 @@ CREATE TABLE IF NOT EXISTS asiento (
     numeroAsiento INT NOT NULL,
     piso INT NOT NULL,
     estado CHAR(1) NOT NULL,
-    placaBus VARCHAR(10),
+    placaBus VARCHAR(10)
 );
 
 CREATE TABLE IF NOT EXISTS rutas (
     idRutas INT AUTO_INCREMENT PRIMARY KEY,
     ciudadOrigen VARCHAR(50) NOT NULL,
-    ciudadFinal VARCHAR(50) NOT NULL,
+    ciudadFinal VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ticket (
@@ -60,5 +60,4 @@ CREATE TABLE IF NOT EXISTS ticket (
     CONSTRAINT fk_pasajero
     FOREIGN KEY (idPasajero)
     REFERENCES pasajero(idPasajero)
-    
 );
